4be1b66e6ef19daf3e036405777ac6d8f29f6715
lfgdevops <miliandevops@gmail.com>
2022-01-27 12:36:51 -0600

    1

--- docs/main.js
+++ docs/main.js
@@ -3,6 +3,7 @@ const serverUrl = "https://cjjnhi1lqgj2.usemoralis.com:2053/server";
 const appId = "AGTU2dcX8JzL0YYE1qHk3hmP38vEcOSJS4Pg7Xad";
 Moralis.start({ serverUrl, appId });
 
+
 /* Authentication code */
 async function login() {
     let user = Moralis.User.current();
